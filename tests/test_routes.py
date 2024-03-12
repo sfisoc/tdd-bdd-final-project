dae336bac6802186e4289ace827b14ee84e2fde2
sfisoc <gammer4live@gmail.com>
2024-03-12 09:28:32 -0400

    added behaviour tests

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -134,16 +134,6 @@ class TestProductRoutes(TestCase):
         # Uncomment this code once READ is implemented
         #
 
-        # # Check that the location header was correct
-         response = self.client.get(location)
-         self.assertEqual(response.status_code, status.HTTP_200_OK)
-         new_product = response.get_json()
-         self.assertEqual(new_product["name"], test_product.name)
-         self.assertEqual(new_product["description"], test_product.description)
-         self.assertEqual(Decimal(new_product["price"]), test_product.price)
-         self.assertEqual(new_product["available"], test_product.available)
-         self.assertEqual(new_product["category"], test_product.category.name)
-
     def test_create_product_with_no_name(self):
         """It should not Create a Product without a name"""
         product = self._create_products()[0]
@@ -194,8 +184,6 @@ class TestProductRoutes(TestCase):
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
         data = response.get_json()
         self.assertIn("was not found", data["message"])
-
-
 
     def test_update_product(self):
         """It should Update an existing Product"""
